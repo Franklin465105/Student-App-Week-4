fd815c998c34cc2a6aaeffce92ab0b1d22a63457
Franklin Esedebe <G00465105@atu.ie>
2025-10-08 17:22:14 +0100

    Storing Information from students

--- src/main/java/ie/atu/week4/StudentApp.java
+++ src/main/java/ie/atu/week4/StudentApp.java
@@ -9,10 +9,26 @@ public class StudentApp {
         int count = 0;
         Scanner sc = new Scanner(System.in);
         List<Student> studentsList = new ArrayList<Student>();
+        Student student1 = new Student();
 
         System.out.println("Please enter the number of students");
-        count = sc.nextInt();
-        //
+        int totalStudents = sc.nextInt();
+
+
+        while (count < totalStudents)
+        {
+            System.out.println("Please enter your name: ");
+            String name = sc.next();
+            System.out.println("Please enter your email: ");
+            String email = sc.next();
+            System.out.println("Please enter your student ID: ");
+            String studentId = sc.next();
+            student1.setName(name);
+            student1.setEmail(email);
+            student1.setStudentId(studentId);
+            studentsList.add(student1);
+            count++;
+        }
 
 
 
