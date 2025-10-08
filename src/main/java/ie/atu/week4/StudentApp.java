f6871eb789e70ef1c2a33d952861aeb577bf5ed4
Franklin Esedebe <G00465105@atu.ie>
2025-10-08 16:51:26 +0100

    Added Array list and three imports.

--- src/main/java/ie/atu/week4/StudentApp.java
+++ src/main/java/ie/atu/week4/StudentApp.java
@@ -1,4 +1,32 @@
 package ie.atu.week4;
+import java.util.List;
+import java.util.Scanner;
+import java.util.ArrayList;
+
 
 public class StudentApp {
+    public static void main(String[] args) {
+        int count = 0;
+        Scanner sc = new Scanner(System.in);
+        List<Student> studentsList = new ArrayList<Student>();
+
+        System.out.println("Please enter the number of students");
+        count = sc.nextInt();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
 }
