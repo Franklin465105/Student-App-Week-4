477a7e10ce8a14a6be3e9ab04761fa3c9a34ff72
Franklin Esedebe <G00465105@atu.ie>
2025-10-22 16:14:55 +0100

    Finished Homework (Had some issues, fixed them in lab)

--- src/main/java/ie/atu/week4/StudentApp.java
+++ src/main/java/ie/atu/week4/StudentApp.java
@@ -3,49 +3,51 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.ArrayList;
 
-
 public class StudentApp {
     public static void main(String[] args) {
         int count = 0;
-        Scanner sc = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         List<Student> studentsList = new ArrayList<Student>();
 
+        System.out.println("Please enter the number of students: ");
+        int totalStudents = input.nextInt();
+        input.nextLine();
 
-        System.out.println("Please enter the number of students");
-        int totalStudents = sc.nextInt();
-        sc.nextLine();
+        while (count < totalStudents) { // Displays the prompt the same amount of times as there are students
+            System.out.print("Please enter your name: ");
+            String name = input.next();
 
-        while (count < totalStudents)
-        {
-            System.out.println("Please enter your name: ");
-            String name = sc.next();
-            System.out.println("Please enter your email: ");
-            String email = sc.next();
-            System.out.println("Please enter your student ID: ");
-            String studentId = sc.next();
-            Student student1 = new Student();
-            student1.setName(name);
-            student1.setEmail(email);
-            student1.setStudentId(studentId);
-            studentsList.add(student1);
-            count++;
+            System.out.print("Please enter your email: ");
+            String email = input.next();
+
+            boolean containsEmail = false;
+            for (Student student : studentsList) {
+                if (student.getEmail().equals(email)) { // Checks if email is in array list
+                    containsEmail = true; // if email is already in arraylist, sets containEmail to true
+                    break;
+                }
+            }
+
+            if (containsEmail) {
+                System.out.println("There is already a student with that email"); // Displays a student already has the email
+            } else {
+                System.out.print("Please enter your student ID: ");
+                String studentId = input.next();
+
+                Student student1 = new Student();
+                student1.setName(name);
+                student1.setEmail(email);
+                student1.setStudentId(studentId);
+                studentsList.add(student1);
+                count++;
+            }
         }
-        for(Student student : studentsList) {
-            System.out.println(student); // "Name: "  +  student.getName() + "Email: " + student.getEmail() + "StudentId: " + student.getStudentId()
+
+        System.out.println("\nStudent List:");
+        for (Student student : studentsList) {
+            System.out.println("Name: " + student.getName() +
+                    ", Email: " + student.getEmail() +
+                    ", Student ID: " + student.getStudentId());
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     }
 }
